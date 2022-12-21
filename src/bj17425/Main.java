71473805adc46d403e80a4ae768f9533d1d1ebe1
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-12-21 23:16:01 +0900

    refactor: 17425 약수의합 -> DP 에라토스테네스의 체

--- src/bj17425/Main.java
+++ src/bj17425/Main.java
@@ -1,25 +1,35 @@
 package bj17425;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
     private static int N;
+    private static Long memo[] = new Long[1_000_001];
 
-    public static void main(String[] args) {
-        N = scanner.nextInt();
-        for (int i = 0; i < N; i++) {
-            int number = scanner.nextInt();
-            long result = getSumOfDivisorsLessThenNumber(number);
-            System.out.println(result);
+    public static void main(String[] args) throws IOException {
+        initMemo();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        N = Integer.parseInt(br.readLine());
+
+        while(N-- > 0) {
+            int input = Integer.parseInt(br.readLine());
+            sb.append(memo[input]).append("\n");
         }
+        System.out.println(sb);
     }
 
-    public static long getSumOfDivisorsLessThenNumber(int number) {
-        long result = 0;
-        for (int i = 1; i <= number; i++) {
-            result += number / i * i;
+    public static void initMemo(){
+        Arrays.fill(memo,0L);
+        for (int i = 1; i <= 1_000_000; i++){
+            for (int j = 1; i*j <= 1_000_000; j++){
+                memo[i*j] += i;
+            }
+            memo[i] += memo[i-1];
         }
-        return result;
     }
 }
